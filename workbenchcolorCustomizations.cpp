dad36afcbe1d1ed119a0aa7adc0c06ea0d78dda0
Iago Munoz <139918975+IagoMunoz@users.noreply.github.com>
2024-07-24 14:04:22 -0300

    Create workbenchcolorCustomizations.cpp

---
+++ workbenchcolorCustomizations.cpp
@@ -0,0 +1,133 @@
+"workbench.colorCustomizations": {
+        
+        //deixando todos os textos branco forte
+        "foreground": "#FFFFFF", // Cor de todos os textos na interface (excluindo o editor)
+        "selection.background": "#FFFFFF",
+        "descriptionForeground": "#FFFFFF",
+        "errorForeground": "#FFFFFF",
+        "textBlockQuote.border": "#FFFFFF",
+        "textLink.foreground": "#FFFFFF",
+        "textPreformat.foreground": "#FFFFFF",
+        "textSeparator.foreground": "#FFFFFF",
+        "button.foreground": "#FFFFFF",
+        "dropdown.foreground": "#FFFFFF",
+        "input.foreground": "#FFFFFF",
+        "input.placeholderForeground": "#FFFFFF",
+        "badge.foreground": "#FFFFFF",
+        "list.activeSelectionForeground": "#FFFFFF",
+        "list.inactiveSelectionForeground": "#ffffff", /////////////////////////////////////////////////////////////
+        "activityBar.foreground": "#FFFFFF",
+        "activityBarBadge.foreground": "#FFFFFF",
+        "sideBar.foreground": "#FFFFFF",
+        "sideBarTitle.foreground": "#FFFFFF",
+        "sideBarSectionHeader.foreground": "#FFFFFF",
+        
+        "tab.unfocusedActiveForeground": "#FFFFFF",
+        "tab.unfocusedInactiveForeground": "#FFFFFF",
+        "breadcrumb.foreground": "#FFFFFF",
+        "menu.foreground": "#FFFFFF",
+        "menu.selectionForeground": "#FFFFFF",
+        "menubar.selectionForeground": "#FFFFFF",
+        "titleBar.activeForeground": "#FFFFFF",
+        "titleBar.inactiveForeground": "#FFFFFF",
+        "notifications.foreground": "#FFFFFF",
+        "notificationLink.foreground": "#FFFFFF",
+        "terminal.foreground": "#FFFFFF",
+        "debugToolBar.background": "#FFFFFF",
+        "welcomePage.buttonBackground": "#FFFFFF",
+        "welcomePage.buttonHoverBackground": "#FFFFFF",
+        "walkThrough.embeddedEditorBackground": "#FFFFFF",
+        "gitDecoration.modifiedResourceForeground": "#FFFFFF",
+        "gitDecoration.deletedResourceForeground": "#FFFFFF",
+        "gitDecoration.untrackedResourceForeground": "#FFFFFF",
+        "gitDecoration.ignoredResourceForeground": "#FFFFFF",
+        "gitDecoration.conflictingResourceForeground": "#FFFFFF",
+        "menu.border": "#ffffff", // Ajuste da cor da borda dos menus secundários
+        "widget.shadow": "#00000000",
+
+
+        // Add custom color settings here if needed
+        "editor.background": "#171717", // muda a cor do fundo do editor
+
+        "editorGutter.background": "#1e1e1e", // Cor da barra lateral das linhas na esquerda
+        "editorLineNumber.foreground": "#ffffff", // Cor dos numeros na barra lateral esquerda
+        "editorStickyScroll.background": "#1e1e1e", // Cor do menuzinho que aparece com a classe ou função que você tá em cima
+
+        "breadcrumb.background": "#171717", // Cor do fundo dos breadcrumbs
+        "breadcrumbPicker.background": "#171717", // Cor do fundo do seletor de breadcrumbs
+        
+        // Menu superior (menubar)
+        "menubar.selectionBackground": "#313131", // Cor do fundo da opção selecionada no menu
+        "titleBar.activeBackground": "#424242", // Cor do fundo do menubar ativo
+        "titleBar.inactiveBackground": "#424242", // Cor do fundo do menubar inativo
+        // Fundo das opções de menu
+        "menu.background": "#424242", // Cor do fundo das opções de menu
+        "menu.selectionBackground": "#424242", // Cor do fundo da opção selecionada no menu
+        "menu.selectionBorder": "#ffffff", // Cor da borda da opção selecionada no menu
+        "menu.separatorBackground": "#FFFFFF", // Cor dos separadores no menu
+
+        "minimap.background": "#1e1e1e", // Cor do fundo da miniatura do código
+
+        "scrollbarSlider.background": "#858585", // Cor do fundo do slider da barra de rolagem
+        "scrollbarSlider.hoverBackground": "#858585", // Cor do fundo do slider da barra de rolagem ao passar o mouse
+        "scrollbarSlider.activeBackground": "#858585", // Cor do fundo do slider da barra de rolagem quando ativo
+        
+        "panel.background": "#1e1e1e", // Cor do fundo do painel inferior
+        "panel.border": "#FFFFFF", // Cor da borda do painel inferior
+        "panelTitle.activeBorder": "#ffffff", // Cor da borda do título ativo no painel inferior
+        "panelTitle.activeForeground": "#ffffff", // Cor do texto do título ativo no painel inferior
+        "panelTitle.inactiveForeground": "#858585", // Cor do texto do título inativo no painel inferior
+
+        "statusBar.background": "#424242", // Cor do fundo da barra de status 
+        "statusBar.foreground": "#ffffff", // Cor do texto na barra de status 
+        "statusBar.border": "#ffffff", // Cor da borda da barra de status 
+        "statusBar.debuggingBackground": "#000000", // Cor do fundo da barra de status ao depurar 
+        "statusBar.debuggingForeground": "#ffffff", // Cor do texto na barra de status ao depurar 
+        "statusBar.debuggingBorder": "#ffffff", // Cor da borda da barra de status ao depurar 
+        "statusBar.noFolderForeground": "#ffffff", // Cor do texto na barra de status quando nenhuma pasta está aberta 
+        "statusBar.noFolderBackground": "#000000", // Cor do fundo da barra de status quando nenhuma pasta está aberta
+        "statusBar.noFolderBorder": "#ffffff", // Cor da borda da barra de status quando nenhuma pasta está aberta 
+        "statusBarItem.activeBackground": "#313131", // Cor do fundo dos itens ativos na barra de status 
+        "statusBarItem.hoverBackground": "#313131", // Cor do fundo dos itens na barra de status ao passar o mouse 
+        "statusBarItem.prominentBackground": "#313131", // Cor do fundo dos itens proeminentes na barra de status
+        "statusBarItem.prominentHoverBackground": "#1e1e1e", // Cor do fundo dos itens proeminentes na barra de status ao passar o mouse 
+        
+        "tab.activeBackground": "#171717", // Cor do fundo da aba ativa
+        "tab.inactiveBackground": "#2d2d2d", // Cor do fundo das abas inativas
+        "tab.activeForeground": "#ffffff", // Cor do texto da aba ativa
+        "tab.inactiveForeground": "#858585", // Cor do texto das abas inativas
+        "tab.hoverBackground": "#313131", // Cor do fundo da aba ao passar o mouse
+        "tab.unfocusedHoverBackground": "#313131", // Cor do fundo da aba não focada ao passar o mouse
+        "tab.border": "#858585", // Cor da borda das abas
+        "tab.activeBorder": "#FFFFFF", // Cor da borda da aba ativa
+        "tab.unfocusedActiveBorder": "#ffffff", // Cor da borda da aba ativa não focada
+        "tab.hoverBorder": "#ffffff", // Cor da borda da aba ao passar o mouse
+        "tab.unfocusedHoverBorder": "#ffffff", // Cor da borda da aba não focada ao passar o mouse
+        "tab.inactiveBorder": "#FF5733", // Cor da borda das abas inativas
+        "editorGroupHeader.tabsBackground": "#202020", // Cor do fundo da barra de títulos do editor (onde ficam as abas e os ícones)
+        "editorGroupHeader.tabsBorder": "#00000000" , // Cor da borda da barra de títulos do editor
+        
+        "sideBar.background": "#202020", // Cor do fundo do menu Explorer
+        "sideBar.border": "#FFFFFF", // Cor da borda do menu Explorer
+        "sideBarSectionHeader.background": "#2D2D2D", // Cor do fundo do cabeçalho das seções no menu Explorer
+        "list.activeSelectionBackground": "#858585", // Cor do fundo do item selecionado ativo
+        "list.inactiveSelectionBackground": "#424242", // Cor do fundo do item selecionado inativo
+        "list.hoverBackground": "#313131", // Cor do fundo do item ao passar o mouse
+        "list.hoverForeground": "#FFFFFF", // Cor do texto do item ao passar o mouse
+        "list.focusBackground": "#313131", // Cor do fundo do item focado
+        "list.focusForeground": "#FFFFFF", // Cor do texto do item focado
+        "list.highlightForeground": "#ffffff", // Cor do texto dos itens destacados
+        
+        "activityBar.background": "#424242", // Cor do fundo da Activity Bar
+        "activityBarBadge.background": "#FF0000", // Cor do fundo dos badges na Activity Bar
+
+        "input.background": "#202020", // Cor do fundo da barra de pesquisa
+        "input.border": "#313131", // Cor da borda da barra de pesquisa
+        "editorSuggestWidget.background": "#202020", // Cor do fundo do menu de sugestões
+        "editorSuggestWidget.foreground": "#FFFFFF", // Cor do texto no menu de sugestões
+        "editorSuggestWidget.border": "#FFFFFF", // Cor da borda do menu de sugestões
+        "editorSuggestWidget.selectedBackground": "#313131", // Cor do fundo do item selecionado no menu de sugestões
+        "editorSuggestWidget.highlightForeground": "#FFAA00", // Cor do texto destacado no menu de sugestões
+        "editorWidget.background": "#2d2d2d", // Cor do fundo geral do menu flutuante
+        "editorWidget.border": "#ffffff" // Cor da borda do menu de pesquisa (Ctrl+F)
+    }
